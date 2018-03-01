4bc0078eeb30b97437739f9f03db310d311e82bc
Niji Ano <fab-bertheol@hotmail.fr>
2018-03-01 10:13:59 +0100

    Add Functions.php
    Add Functions.php

---
+++ src/JsonDB/Functions.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * JsonDB
+ *
+ * @project: JsonDB Projects
+ * @purpose: DB functions for JsonDB
+ * @version: 1.0
+ *
+ * @author: Niji, Ano
+ * @created on: 01 Mar, 2018
+ *
+ * @url: https://anoniji.com
+ * @license: Creative Commons
+ *
+ */
+
+namespace JsonDB
+{
+	class Functions
+	{
+		protected $er;
+
+		public function Format()
+	    {	
+	    	// In development
+	    }
+
+	    public function Date()
+	    {	
+	    	// In development
+	    }
+
+	    public function Mail()
+	    {
+	    	// In development
+	    }	   
+
+	    public function Number()
+	    {
+	    	// In development
+	    }
+
+	    public function Check()
+	    {
+	    	// In development
+	    }
+
+	    public function Protect()
+	    {
+	    	// In development
+	    }
+
+	    /**
+	     * @return Error
+	     */
+	   	public function getError()
+	    {
+	        return $this->er;
+	    }
+	}
+}
+?>
